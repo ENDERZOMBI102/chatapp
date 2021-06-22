9ae1f14d2cf17a8228f068afa2ec6fe4e64ec2a6
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-06-22 23:03:57 +0200

    fix crash when closing WS connection

--- src/server/WSClientHandler.py
+++ src/server/WSClientHandler.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from websockets.exceptions import ConnectionClosedError
 from websockets.legacy.server import WebSocketServerProtocol
 
 from BaseClientHandler import BaseClientHandler
@@ -20,9 +21,12 @@ class WSClientHandler(BaseClientHandler):
 		await self.wsocket.send( await self.ReplacePlaceholders( message )  )
 
 	async def InputLoop( self ) -> None:
-		while self.alive and not self.wsocket.closed:
-			async for msg in self.wsocket:
-				await self.HandleMessage(msg)
+		try:
+			while self.alive and not self.wsocket.closed:
+				async for msg in self.wsocket:
+					await self.HandleMessage(msg)
+		except ConnectionClosedError:
+			pass
 		self.alive = False
 
 	async def CheckErrors( self ) -> None:
