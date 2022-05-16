175463d87bde3b0b5729a5de02f72ab3943a29e0
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-05-16 11:44:46 +0200

    actually delete a character when backspace is pressed print traceback + exception on unhandled exception

--- src/client/newcli.py
+++ src/client/newcli.py
@@ -1,13 +1,20 @@
 import curses
+from curses import ascii
 import optparse
 import os
+import traceback
 from queue import Queue
 
 from data import Message
 from .Client import Client
 
 
-def main(scrn):
+class Options:
+	username: str
+	address: str
+
+
+def main( scrn ):
 	curses.start_color()
 	parser = optparse.OptionParser()
 
@@ -27,35 +34,32 @@ def main(scrn):
 		help='use this server instead of the default one, format: host:port',
 		default='127.0.0.1:20307'
 	)
-	class Options:
-		username: str
-		address: str
 
 	options, args = parser.parse_args()  # type: Options, list[str]
 
 	inboundMessageQueue: Queue[ Message ] = Queue()
 	line: int = 0
 
-	client: Client = Client()
-	client.setMessageListener( lambda msg: inboundMessageQueue.put(msg) )
+	with Client() as client:
+		client.setMessageListener( inboundMessageQueue.put )
 
-	host, port = options.address.split(':')
-	client.setAddress( host, port )
-	client.setUsername( options.username )
+		host, port = options.address.split(':')
+		client.setAddress( host, port )
+		client.setUsername( options.username )
 
-	scrn.addstr( 0, 0, f'Logged in as {options.username}' )
-	# event loop
-	try:
+		scrn.addstr( 0, 0, f'[SYSTEM] Logged in as {options.username}' )
+		# event loop
 		scrn.nodelay(True)
-		curses.nl()
 		string = ''
 		while True:
 			c = scrn.getch()
 			if 255 > c > -1:
 				string += chr(c)
-			
+			if c == ascii.BS:
+				string = string[:-2]
+
 			if string.endswith('\n'):
-				string = string[:-1]
+				string = string[ :-1 ]
 				if string.startswith(':'):
 					string = string[ 1: ]
 					if string == 'q':
@@ -67,26 +71,23 @@ def main(scrn):
 					scrn.addstr( line, 0, string )
 					client.send( string )
 					string = ''
-					
+
 			for i in range( inboundMessageQueue.qsize() ):
 				msg = inboundMessageQueue.get()
 				line += 1
-				scrn.addstr( line, 0, msg )
+				scrn.addstr( line, 0, msg.content )
 				inboundMessageQueue.task_done()
-			
+
 			scrn.move( line + 1, 0 )
 			scrn.deleteln()
 			scrn.addstr( line + 1, 0, '> ' + string )
-	finally:
-		del client
 
 
 def main0() -> None:
 	try:
 		curses.wrapper(main)
 	except Exception as e:
-		print(e)
-		input()
+		traceback.print_exception( type(e), e, e.__traceback__ )
 
 
 if __name__ == '__main__':
